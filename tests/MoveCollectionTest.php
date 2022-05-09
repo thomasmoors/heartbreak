87cdfbea03446737a9e1ca79000c6f771b67da86
Thomas Moors <thomas.moors@proautnorm.com>
2022-05-09 14:31:52 +0200

    Test points

--- tests/MoveCollectionTest.php
+++ tests/MoveCollectionTest.php
@@ -22,6 +22,18 @@ class MoveCollectionTest extends TestCase
         $moves->add($highest);
 
         $this->assertSame($moves->moveWithHighestMatchingCard(), $highest);
+    }
+
+    public function testTotalPoints()
+    {
+        $moves = new MoveCollection();
+
+        $moves->add(new Move(new Card(Suit::Hearts, 11), new Player('A'))); // 1
+        $moves->add(new Move(new Card(Suit::Clubs, 11), new Player('B'))); //
+        $moves->add(new Move(new Card(Suit::Spades, 12), new Player('C')));
+        $moves->add(new Move(new Card(Suit::Clubs, 7), new Player('D')));
+
+        $this->assertEquals(8, $moves->points());
 
     }
 }
